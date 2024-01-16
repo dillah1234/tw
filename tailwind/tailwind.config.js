0bb0ba6f6f281c4c4c52c5227a7729e32a3a667f
dillah1234 <arrafif0722@abudzar.sch.id>
2024-01-16 11:03:35 +0700

    up

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {fontFamily:{
+      "Montserrat": "Montserrat",
+    },
+    fontSize: {
+      agakbesar: "150px",
+    },
+  },
+  plugins: [],
+}
+}
